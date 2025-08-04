e87ed07cbfbfa14c0135e3edc21f0ac3bf2310e9
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-08-04 18:59:44 -0500

    change the gameBoard array to be an object

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const gameBoard = [[],[],[]];
+let gameBoard = {
+    board:["","","","","","","","",""],
+}
 
-
+let winningPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+]
 const optionPicker = () =>{
    let options = ["X","O"];
    let  computerOption = options[Math.floor(Math.random() * 2)];
@@ -12,7 +23,8 @@ const optionPicker = () =>{
    else{
     userOption = options[0];
    }
-   console.log(computerOption);
-   console.log(userOption);
+  gameBoard.push(userOption);
+  gameBoard.push(computerOption);
+  
    
-}
+}   
