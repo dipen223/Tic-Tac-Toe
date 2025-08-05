a4fd0b29adfc4dbb034e819ee55a41a228328063
Dipen Bhandari <dipenbhandari205@outlook.com>
2025-08-04 21:49:17 -0500

    Add a logic to choose a symbol and spot for a player and a computer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let gameBoard = {
     board:["","","","","","","","",""],
-}
 
+}
 let winningPatterns = [
     [0,1,2],
     [0,3,6],
@@ -12,19 +12,38 @@ let winningPatterns = [
     [3,4,5],
     [6,7,8],
 ]
-const optionPicker = () =>{
-   let options = ["X","O"];
-   let  computerOption = options[Math.floor(Math.random() * 2)];
-   let userOption;
-   if(computerOption == options[0]){
-    userOption = options[1];
 
-   }
-   else{
-    userOption = options[0];
-   }
-  gameBoard.push(userOption);
-  gameBoard.push(computerOption);
-  
-   
-}   
+function chooseSymbol(){
+    let playerSymbol = prompt("Please pick your symbol: X or O ?");
+    let computerSymbol;
+
+    if(playerSymbol === "X"){
+        computerSymbol = "O";
+    }
+    else{
+        computerSymbol = "X";
+    }
+    
+    function chooseSpot(){
+    let userSpot = prompt("Please enter a spot number to place your symbol");
+    gameBoard.board[userSpot] = playerSymbol;
+    let computerSpot = Math.floor(Math.random() * gameBoard.board.length);
+    console.log(computerSpot);
+    if(computerSpot != userSpot){
+        gameBoard.board[computerSpot] = computerSymbol;
+    
+
+    }
+}
+chooseSpot();
+
+}
+
+
+
+
+
+
+// based on the choice of player,computer has options on board to choose random cells from the remaining cells
+// let's say I start the game and pick one cell to place
+// In computer turn it chooses one random cell on it's turn
